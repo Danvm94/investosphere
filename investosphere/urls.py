242c3c991106a114f62d04512b587ff454b22965
danvm94 <danvm94@gmail.com>
2023-08-09 19:35:17 +0100

    Add nav bar on the top with small functions Added register.html and base.html templates register.html still need work for form.username and others mandatory for register

--- investosphere/urls.py
+++ investosphere/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("user_management/", include("user_management.urls")),
+    path("register/", include("user_management.urls")),
     path('admin/', admin.site.urls),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
-    
+def registration_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # Redirect to the home page or another appropriate URL
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
