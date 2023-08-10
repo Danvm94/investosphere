fc7e559e37f69a3e2c68b82e877ddd4f9d48b24f
danvm94 <danvm94@gmail.com>
2023-08-10 14:35:20 +0100

    Add warning messages style and JS function so that the user can close the message if needed for better UI

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,32 @@
 function allowOnlyLetters(inputElement) {
   inputElement.value = inputElement.value.replace(/[^a-zA-Z]/g, "");
 }
+
+var successMessages = document.getElementsByClassName("alert-success");
+
+// Set the timeout to remove the div after 5 seconds (5000 milliseconds)
+setTimeout(function () {
+  for (var i = 0; i < successMessages.length; i++) {
+    successMessages[i].style.opacity = "0"; // Fade out animation
+  }
+}, 8000); // Start fading after 800ms
+
+setTimeout(function () {
+  for (var i = 0; i < successMessages.length; i++) {
+    successMessages[i].style.display = "none"; // Hide completely after animation
+  }
+}, 10000); // Hide completely after 10 seconds
+
+function opacityChange(type, element) {
+  if (type === "hide") {
+    element.style.opacity = "0";
+    setTimeout(() => {
+      element.style.display = "none";
+    }, 1800);
+  } else {
+    element.style.display = "flex";
+    setTimeout(() => {
+      element.style.opacity = "1";
+    }, 50);
+  }
+}
