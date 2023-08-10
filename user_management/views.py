2bdedfa243c88ac18ca66e2dbad683dd1fee2ed1
danvm94 <danvm94@gmail.com>
2023-08-10 11:05:40 +0100

    Add message div to the base.html using jinja to display any possible errors, this is now used on registration form to notify user if it failed to proceed with the registration

--- user_management/views.py
+++ user_management/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -10,10 +11,21 @@ def registration_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, 'Registration successful!')
             # Redirect to the home page or another appropriate URL
-            return redirect('home')
+            return redirect('/')
     else:
         form = RegistrationForm()
+
+    # If form is not valid, collect field-specific error messages
+    field_errors = []
+    for field_name, errors in form.errors.items():
+        for error in errors:
+            field_errors.append(f'{form.fields[field_name].label}: {error}')
+    if field_errors:
+        for field_error in field_errors:
+            messages.warning(request, field_error)
+
     return render(request, 'register.html', {'form': form})
 
 
