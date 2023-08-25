1bdc8eb6b2fb9a3cbf46e93578d269419cdb6152
danvm94 <danvm94@gmail.com>
2023-08-25 09:35:49 +0000

    JS script for amountInput working, it displays a user friendly number for the input and converts it back to decimal on submission

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -31,16 +31,46 @@ function opacityChange(type, element) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   var newsCarouselElement = document.getElementById("newsCarousel");
   // Check if the newsCarousel element exists
   if (newsCarouselElement !== null) {
-      var newsCarousel = new bootstrap.Carousel(newsCarouselElement, {
-          interval: 3000, // Change slide every 3 seconds (adjust as needed)
-          wrap: true, // Set to false if you don't want to loop
-      });
+    var newsCarousel = new bootstrap.Carousel(newsCarouselElement, {
+      interval: 3000, // Change slide every 3 seconds (adjust as needed)
+      wrap: true, // Set to false if you don't want to loop
+    });
   }
 
-  var amountInput = document.getElementById('amountInput');
-  
+  var amountInput = document.getElementById("id_dollars");
+  if (amountInput !== null) {
+    let inputBuffer = "0.00";
+
+    amountInput.addEventListener("input", function (event) {
+      const value = event.target.value;
+
+      // Remove non-digit characters from the input
+      const digits = value.replace(/\D/g, "");
+
+      // Convert the digits into a floating-point number
+      const amount = parseFloat(digits) / 100;
+
+      // If amount is NaN, set it to 0
+      const sanitizedAmount = isNaN(amount) ? 0 : amount;
+
+      // Format the number as currency with two decimal places
+      const formattedAmount = amount.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      // Update the input value
+      event.target.value = formattedAmount;
+    });
+    // Handle form submission to convert the formatted value back to decimal before sending
+    amountInput.closest("form").addEventListener("submit", function () {
+      const formattedValue = amountInput.value;
+      const decimalValue = parseFloat(formattedValue.replace(/[^0-9.-]/g, ""));
+      amountInput.value = decimalValue.toFixed(2); // Set the value back to decimal format
+    });
+  }
 });
