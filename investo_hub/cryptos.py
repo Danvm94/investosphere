0e066678a7632c67c4e36e3a4dc350e32ee00cad
danvm94 <danvm94@gmail.com>
2023-09-05 08:38:53 +0100

    Fix cryptos.py cache inconsistency
    Resolved a bug in cryptos.py where the cache set and load operations used different names, causing issues in the code.

--- investo_hub/cryptos.py
+++ investo_hub/cryptos.py
@@ -21,7 +21,7 @@ def request_coin_api():
         response = requests.get(COIN_API + '/coins/markets', params=params)
         if response.status_code == 200:
             cryptos_cache = response.json()
-            cache.set('cached_cryptos_trending', cryptos_cache, 12 * 60 * 60)
+            cache.set('cached_cryptos', cryptos_cache, 12 * 60 * 60)
             return cryptos_cache
         else:
             time.sleep(5)
@@ -37,7 +37,6 @@ def request_coin_cache():
 def get_coin_info(coin):
     cryptos_cache = request_coin_cache()
     for crypto in cryptos_cache:
-        print(coin)
         if crypto['id'] == coin:
             return crypto
     raise Exception(f'Cryptocurrency "{coin}" not found in the cache.')
@@ -46,6 +45,7 @@ def get_coin_info(coin):
 def get_user_cryptos(user):
     cryptos = Cryptos.objects.filter(user=user)
     for crypto in cryptos:
+        print(crypto.symbol)
         crypto.usd = Decimal(get_coin_info(crypto.symbol)['current_price']) * Decimal(crypto.amount)
         crypto.usd = round(crypto.usd, 2)
         crypto.image = get_coin_info(crypto.symbol)['image']
