4185a2dcb75a8b085daa1363bd3547f81812af24
danvm94 <danvm94@gmail.com>
2023-09-08 08:55:55 +0100

    Heroku deployment fix, CSS and JS not rendering on Heroku deployed page.

--- investosphere/settings.py
+++ investosphere/settings.py
@@ -90,11 +90,9 @@ if os.environ.get("DEBUG") == "True":
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-    STATIC_URL = 'static/'
 else:
     DATABASES = {'default': dj_database_url.parse(
         os.environ.get("DATABASE_URL"))}
-    STATIC_URL = 'https://res.cloudinary.com/dtja957k3'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -127,12 +125,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_URL = '/investosphere/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_URL = '/media/'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
